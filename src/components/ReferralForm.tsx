4c378f4df02a496943aa91d8dc3aba9d706846c3
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2025-07-06 22:54:44 +0800

    fix: title

--- src/components/ReferralForm.tsx
+++ src/components/ReferralForm.tsx
@@ -76,7 +76,7 @@ const ReferralForm = () => {
     <div className="tsmc-form-container">
       {isSubmitting && <LoadingSpinner />}
       <form ref={formRef} action="" onSubmit={handleSubmit}>
-        <h1> Referral </h1>
+        <h1> TSMC Referral </h1>
         <NameInput name="user_name" value={name} onChange={setName} />
         <EmailInput name="user_email" value={email} onChange={setEmail} />
         <LocationInput
