d37894f4d2370da70164c3d2b883a59b6dcbe3a2
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2025-07-07 08:29:54 +0800

    feat: add vault

--- src/components/ReferralForm.tsx
+++ src/components/ReferralForm.tsx
@@ -47,6 +47,10 @@ const ReferralForm = () => {
       const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID || "";
       const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID || "";
       const PUBLIC_KEY = process.env.REACT_APP_EMAILJS_PUBLIC_KEY || "";
+      console.log("SERVICE_ID", SERVICE_ID);
+      console.log("TEMPLATE_ID", TEMPLATE_ID);
+      console.log("PUBLIC_KEY", PUBLIC_KEY);
+
 
       await emailjs.sendForm(
         SERVICE_ID,
